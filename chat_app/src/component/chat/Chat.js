78cacd5d0535e4a148c9e20a1650e2eeca5880cd
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-08 21:11:41 +0600

    create chat and message component

--- chat_app/src/component/chat/Chat.js
+++ chat_app/src/component/chat/Chat.js
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ReactScrollToBottom from "react-scroll-to-bottom";
+import socketIo from "socket.io-client";
+import closeIcon from "../../images/closeIcon.png";
+import sendLogo from "../../images/send.png";
+import { user } from "../join/Join";
+import Message from "../Message/Message";
+import "./chat.css";
+
+let socket;
+
+const ENDPOINT = "https://demo-cchat.herokuapp.com/";
 
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
-}
+  const [id, setid] = useState("");
+  const [messages, setMessages] = useState([]);
 
-export default Chat
+  const send = () => {
+    const message = document.getElementById("chatInput").value;
+    socket.emit("message", { message, id });
+    document.getElementById("chatInput").value = "";
+  };
+
+  console.log(messages);
+  useEffect(() => {
+    socket = socketIo(ENDPOINT, { transports: ["websocket"] });
+
+    socket.on("connect", () => {
+      alert("Connected");
+      setid(socket.id);
+    });
+    console.log(socket);
+    socket.emit("joined", { user });
+
+    socket.on("welcome", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    socket.on("userJoined", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    socket.on("leave", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [messages]);
+
+  useEffect(() => {
+    socket.on("sendMessage", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message, data.id);
+    });
+    return () => {
+      socket.off();
+    };
+  }, [messages]);
+
+  return (
+    <div className="chatPage">
+      <div className="chatContainer">
+        <div className="header">
+          <h2>C CHAT</h2>
+          <a href="/">
+            {" "}
+            <img src={closeIcon} alt="Close" />
+          </a>
+        </div>
+        <ReactScrollToBottom className="chatBox">
+          {messages.map((item, i) => (
+            <Message
+              key={i}
+              user={item.id === id ? "" : item.user}
+              message={item.message}
+              classs={item.id === id ? "right" : "left"}
+            />
+          ))}
+        </ReactScrollToBottom>
+        <div className="inputBox">
+          <input
+            onKeyPress={(event) => (event.key === "Enter" ? send() : null)}
+            type="text"
+            id="chatInput"
+          />
+          <button onClick={send} className="sendBtn">
+            <img src={sendLogo} alt="Send" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
