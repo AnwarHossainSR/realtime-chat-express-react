701bef7cbce796188fd3ba17c7bb1c57a7b0b6a9
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-08 21:02:15 +0600

    create join component

--- chat_app/src/component/join/Join.js
+++ chat_app/src/component/join/Join.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.png";
+import "./join.css";
+
+let user;
+
+const sendUser = () => {
+  user = document.getElementById("joinInput").value;
+  document.getElementById("joinInput").value = "";
+};
 
 const Join = () => {
-  return <div>Join</div>;
+  const [name, setname] = useState("");
+
+  return (
+    <div className="JoinPage">
+      <div className="JoinContainer">
+        <img src={logo} alt="logo" />
+        <h1>C CHAT</h1>
+        <input
+          onChange={(e) => setname(e.target.value)}
+          placeholder="Enter Your Name"
+          type="text"
+          id="joinInput"
+        />
+        <Link
+          onClick={(event) => (!name ? event.preventDefault() : null)}
+          to="/chat"
+        >
+          {" "}
+          <button onClick={sendUser} className="joinbtn">
+            Login In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
+export { user };
